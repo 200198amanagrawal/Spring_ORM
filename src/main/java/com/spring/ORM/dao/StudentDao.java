7e7e4a4e25ce9741ecc7059c4eb7d3b674b4124b
amanagrawal <amanagrawal2098@gmail.com>
2022-05-17 01:06:02 +0530

    added crud functions

--- src/main/java/com/spring/ORM/dao/StudentDao.java
+++ src/main/java/com/spring/ORM/dao/StudentDao.java
@@ -1,5 +1,7 @@
 package com.spring.ORM.dao;
 
+import java.util.List;
+
 import javax.transaction.Transactional;
 
 import org.springframework.orm.hibernate5.HibernateTemplate;
@@ -18,11 +20,36 @@ public class StudentDao {
 		this.hibernateTemplate = hibernateTemplate;
 	}
 
+	/*
+	 * CRUD based functions which will allow to create,read,update and delete.
+	 * */
 	@Transactional
 	//save student
 	public int insert(Student student)
 	{
 		//inserts student
 		return (Integer) hibernateTemplate.save(student);
+	}
+	
+	public Student getStudent(int id)
+	{
+		return hibernateTemplate.get(Student.class, id);
+	}
+	
+	public List<Student> getAllStudents()
+	{
+		return hibernateTemplate.loadAll(Student.class);
+	}
+	
+	@Transactional
+	public void delete(int id)
+	{
+		hibernateTemplate.delete(getStudent(id));
+	}
+	
+	@Transactional
+	public void update(Student student)
+	{
+		hibernateTemplate.update(student);
 	}
 }
